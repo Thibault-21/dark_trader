7dcc99a915b5256b12dd419a472c7764489a6a68
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-18 20:58:30 +0200

    update

--- mairie_christmas.rb
+++ mairie_christmas.rb
@@ -2,13 +2,18 @@
 require 'rubygems'
 require 'nokogiri'   
 require 'open-uri'
-PAGE_URL = "http://annuaire-des-mairies.com/"
-page = Nokogiri::HTML(open(PAGE_URL)) 
 
 def get_townhall_email(townhall_urls)
+=begin 
+    je créé une array qui = ma variable ; j'utilise la fonction map pour faire une boucle afin de 
+    pouvoir recollecter mes informations.
+=end
     townhall_email = townhall_urls.map { |page| Nokogiri::HTML(open(page)).xpath('//tr//td[contains(text(),"@")]').text }
+    
     townhall_name_raw = townhall_urls.map { |page| Nokogiri::HTML(open(page)).xpath('/html/body/div/main/section[1]/div/div/div/h1').text }
+    
     townhall_name = townhall_name_raw.map { |town| town.capitalize.split.take(1).join}
+    
     h = Hash[townhall_name.zip(townhall_email)]
     result = [h.each {|k,v| Hash[k => v] }]
     puts result.inspect
@@ -40,16 +45,6 @@ def get_townhall_email(townhall_urls)
   # a = ["ABLEIGES - 95450", "ROUBAIX - 150000"]
   # a.map {|commune| puts commune.capitalize.split.take(1).join.inspect }
 
-  - ------------------------------------- - 
 
-def get_town_hall_mail(town_hall_urls)
-    val_oise_emails = town_hall_url.map {|page|.xpath('//tr//td[contains(text(),"@")]').text }
-    
-    end
-    val_oise_ville
-    page.xpath('//a[contains(@class, "lientxt")]').each do |elem|
-        val_oise_ville << elem.text 
-    end
-    
-def get_townhall_urls
+
 
