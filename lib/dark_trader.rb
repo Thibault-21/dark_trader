d262599daa32504e98d4f5626a1a2b0fc30a23d5
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-18 16:14:25 +0200

    update

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -3,10 +3,49 @@ require 'nokogiri'
 require 'open-uri'
 
    page = Nokogiri::HTML(open('https://www.https://coinmarketcap.com/all/views/all/'))
-   
+----------------------------------------------
+   require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+def scraping(page)
+ # Scraping des symboles de cryptos
+ currencies_scrap = page.xpath('//td[contains(concat(" ",normalize-space(@class)," "), " col-symbol ")]')
+ currencies = currencies_scrap.map { |currency| currency.text.strip } 
+ # Scraping des valeurs de cryptos
+ values_scrap = page.xpath('//a[contains(concat(" ",normalize-space(@class)," "), " price ")]')
+ values = values_scrap.map { |value| value.text.delete("$").to_f } # retirer le symbole $ text.delete; pour le convertir, on utilise le float pour avoir un nombre numérique, décimal.
+ # Regroupement des deux arrays en un hash
+ h = Hash[currencies.zip(values)]
+ # Segmentation du hash en plusieurs hashes au sein d'un array
+ result = [h.each {|k,v| Hash[k => v] }]
+
+ puts result.inspect
+end
+
+scraping(page)
+
+-------------
+
+   crypto_currencies = Array.new #tout le tableau des cryptos
+# crypto_currencies //*[@id="currencies"]
+    currencies = page.xpath('//tbody/a')
+
+    # code xpath du nom bitcoin 
    crypto_names = Array.new
-   crypto_names //*[@id="currencies"]/tbody  
-  
+   crypto_names //*[@id="th-name"]
+
+   #nom du fournisseur (BTC)
+   crypto-names = Array.new
+   crypto_names //*[@id="th-totalsupply"]
+   
+    # code xpath du prix du bitcoin 
+   crypto_cours = Array.new 
+   crypto_cours //*[@id="th-price"]
+
+     
+   
 
 
 
