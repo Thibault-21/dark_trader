eb2e4a9617f9099a06c90dcbf5d97018ac309ef0
Thibault-21 <43401546+Thibault-21@users.noreply.github.com>
2019-07-18 18:04:32 +0200

    update

--- mairie_christmas.rb
+++ mairie_christmas.rb
@@ -9,12 +9,13 @@ page = Nokogiri::HTML(open(PAGE_URL))
 val_oise_emails = Array.new
 val_oise_ville = Array.new 
 
-page.xpath('//a[contains(@href, "mailto")]')).each do |elem|
-
-/html/body/div[1]/main/section[2]/div/table/tbody/tr[21]/td[4]/a #copy path du Val d'Oise 
-
-/html/body/div[1]/main/section[2]/div/table/tbody/tr[1]/td[1]/a 
 
 def scrapping(page) 
 
-    all_emails_links = page.xpath('/mettre_ici_le_XPath')
+    page.xpath('//a[contains(@href, "mailto")]')).each do |elem|
+        val_oise_emails << elem.text
+    end
+
+    page.xpath('//td[contains(@class, "lientxt")]').each do |elem|
+        val_oise_ville << elem.text 
+    end
